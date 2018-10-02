f393e6f2c8ad9bf2da2900a79a6e953fd558c967
c-beltran <carlosbeltran90@gmail.com>
2018-10-01 23:27:17 -0400

    added loading animation to loading bars

---
+++ portfolio.js
@@ -0,0 +1,58 @@
+/*loading bar animation*/
+
+//this function gets the height of the whole html document
+function getDocHeight() {
+  var doc = document;
+  return Math.max(
+    doc.body.scrollHeight, doc.documentElement.scrollHeight,
+    doc.body.offsetHeight, doc.documentElement.offsetHeight,
+    doc.body.clientHeight, doc.documentElement.clientHeight
+    )
+}
+
+//this calculates the amount the person scrolls down and up a page
+function amountscrolled(){
+  var html = document.getElementById("html");
+  var css = document.getElementById("css");
+  var js = document.getElementById("js");
+  var node = document.getElementById("node");
+  var java = document.getElementById("java");      
+  var ror = document.getElementById("ruby");
+  var git = document.getElementById("git");
+  var id = setInterval(frame, 10);
+
+  var winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+  var docheight = getDocHeight()
+  var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+  var trackLength = docheight - winheight
+    var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+    console.log(pctScrolled + '% scrolled')
+
+    //if person reaches a certain length down a page than we show the progress bars
+    function frame() {
+      if (pctScrolled >= 100) {
+        clearInterval(id);
+      }
+      else if(pctScrolled == 13){
+        html.style.width = 90 + '%';
+        html.style.display = "block";
+        css.style.width = 70 + '%';
+        css.style.display = "block";
+        js.style.width = 73 + '%';
+        js.style.display = "block";
+        node.style.width = 73 + '%';
+        node.style.display = "block";
+        java.style.width = 70 + '%';
+        java.style.display = "block";
+        ror.style.width = 50 + '%';
+        ror.style.display = "block";
+        git.style.width = 90 + '%';
+        git.style.display = "block";
+      } 
+    }    
+  }
+
+  window.addEventListener("scroll", function(){
+    amountscrolled()
+  }, false)
+
